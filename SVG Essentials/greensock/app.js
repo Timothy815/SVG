f29b0506cf67de7b3f5df669e37a44d791ece1d6
Tim Koerner <tdmkoerner@gmail.com>
2022-11-27 14:40:01 -0600

    Add greensock example

--- SVG Essentials/greensock/app.js
+++ SVG Essentials/greensock/app.js
@@ -3,9 +3,9 @@ let reverse = document.querySelector("#rev");
 let pause = document.querySelector("#pause");
 let resume = document.querySelector("#resume");
 let restart = document.querySelector("#restart");
-let tween; 
+let tween=gsap.to("#circle", {duration: 1, x: 200, scale:0.5, ease: "power1",repeat:2,yoyo:true ,paused:true});
 start.addEventListener("click",()=>{
-   tween = gsap.to("#circle", 2.3, {x: 200, scale:0.5 });
+   tween.play();
 });
 
 
